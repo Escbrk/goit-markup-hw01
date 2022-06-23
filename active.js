df9ee28fb403881099bcca69ec6abf83a43cede0
Escobarek <69437294+Escobarek@users.noreply.github.com>
2022-06-23 22:19:44 +0200

    Create active.js

---
+++ active.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.nav-category-btn');
+const pages = document.querySelectorAll('.category-item');
+
+buttons.forEach(button => {
+  button.addEventListener("click", e => {
+    buttons.forEach(button => {
+      button.classList.remove("active-button");
+    })
+    e.target.classList.add("active-button");
+    pages.forEach(project => {
+      let type = project.querySelector('.category-text').innerText;
+      if (type == e.target.innerText || e.target.innerText == 'Все') {
+        project.classList.remove('invisivisually-hiddenble');
+      } else {
+        project.classList.add('visually-hidden');
+      }
+    });
+  })
+})
